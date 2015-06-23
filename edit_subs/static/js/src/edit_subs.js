9bcc99a2b6f202e7bae631ed7336b76bc659df31
raipc <anton-rib@mail.ru>
2015-06-23 19:40:58 +0300

    Revert "Improved activation/deactivation logic"
    This reverts commit 6dd070d25669e455c76d4d443c94a262e2ee9734.

--- edit_subs/static/js/src/edit_subs.js
+++ edit_subs/static/js/src/edit_subs.js
@@ -6,7 +6,6 @@ function EditSubsXBlock(runtime, element) {
     var $activateBtn;
     var $subtitlesBtn;
     var $defSubtitleBtn;
-    var $hideSubtitlesBtn;
     var $subtitleString;
     var changedSubtitleString; //Value before editing
     var isEditingActive = false;
@@ -66,13 +65,14 @@ function EditSubsXBlock(runtime, element) {
     }
 
     function _requestHelper(serverHandler, sendData, callback){
-        $.post(runtime.handlerUrl(element, serverHandler), JSON.stringify(sendData))
+        $.post(runtime.handlerUrl(element, serverHandler),
+            JSON.stringify(sendData))
             .done(function(data){
                 if(data.result === "success"){
                     callback(data.data);
                 }
-                else if(data.result === "error") {
-                   showError(data.message);
+                else {
+                    console.log(data.message);
                 }
             });
     }
@@ -81,58 +81,9 @@ function EditSubsXBlock(runtime, element) {
         subtitleData = new Esjson(data);
     }
 
-    function showMessage(messageClass, message){
-        var $messageField = $('<div/>').attr("class","es-message-container").append(
-            $('<span/>').attr("class", "es-message " + messageClass).html(message)
-        );
-        $messageField.appendTo($videoElement);
-        setTimeout(function(){$messageField.remove();}, 3000);
-    }
-
-    function showError(message){
-        showMessage("es-error-message", message);
-    }
-    function showResult(message){
-        showMessage("es-result-message", message);
-    }
-
-    function rate(subtitleIndex, serverHandler){
-        _requestHelper(
-            serverHandler,
-            {
-                "subtitle_id": subtitleData.subtitle_id[subtitleIndex]
-            },
-            showResult
-        )
-    }
-
-    function subtitleUpdateHandler(data){
-        if(isEditingActive)
-            return $videoElement.trigger("deferredUpdate", data);
-        var index = subtitleData.start.indexOf(data.start);
-        if (index < 0) return;
-        if (subtitleData.rating[index] > data.rating) return;
-        else if (subtitleData.rating[index] === data.rating
-            && new Date(subtitleData.updated_at[index]) > new Date(data.updated_at) )
-            return;
-        subtitleData.text[index] = data.text;
-        subtitleData.subtitle_id[index] = data.subtitle_id;
-        subtitleData.rating[index] = data.rating;
-        subtitleData.updated_at = data.updated_at;
-    }
-
-    function repositoriesUpdateHandler(data) {
-        if (!data.repo_id in repos.repo_id){
-            repos.repo_id.push(data.repo_id);
-            repos.repo_name.push(data.repo_name);
-            repos.description.push(data.description);
-        }
-        //TODO: Append to DOM
-    }
-
     $(function ($) {
         var $container = $(".editsubs-container", element);
-        var videoId = $container.data("videoid");
+        videoId = $container.data("videoid");
         $videoElement = $(".video").filter(function() {
             return $(this).data("metadata").sub === videoId;
         });
@@ -149,14 +100,9 @@ function EditSubsXBlock(runtime, element) {
             alert("Video not found");
 
 
-        /*
-            Handle events only after activating module
-         */
         $activateBtn.on("click",".es-deactivated", function(){
-            _requestHelper("activate", {}, function(data){
-                repos = new Repos(data.repos);
-                loadSubtitles(data.subs);
-            });
+            getReposRequest(videoId);
+            //TODO activate
 
             $(".video").on("caption:update", updateSubtitle);
 
@@ -170,104 +116,124 @@ function EditSubsXBlock(runtime, element) {
                     .appendTo($defSubtitleBtn.parent());
             }
             $(this).removeClass("es-deactivated").addClass("es-activated");
-
-
-            $(".editsubs-operation-cancel", $videoElement).click(function(){
-                $subtitleString.text(changedSubtitleString);
-                isEditingActive = false;
-            });
-
-            $(".editsubs-operation-save", $videoElement).click(function(){
-                _requestHelper(
-                    "add_subtitle",
-                    JSON.stringify({
-                        "text": $subtitleString.text(),
-                        "start": subtitleData.start[$subtitleString.data("index")],
-                        "end": subtitleData.end[$subtitleString.data("index")]
-                    }),
-                    function(data){}
-                )
-            });
-
-            $(".editsubs-string", $videoElement).on('input', function(){
-                if(isEditingActive) return;
-                $subtitleString = $(this);
-                changedSubtitleString = $subtitleString.text();
-                isEditingActive = true;
-            });
-
-            $(".es-rate-positive:not(.es-rated)", $videoElement)
-                .on("click", function(){
-                    rate($subtitleString.data("index"), "rate_positive");
-                });
-
-            $(".es-rate-negative:not(.es-rated)", $videoElement).on("click",
-                function(){
-                    rate($subtitleString.data("index"), "rate_negative");
-                });
-
-            $(".editsubs-repo", $videoElement).on("click", function(){
-                _requestHelper(
-                    "switch_to_repository",
-                    {"repo_id": $(this).data("repo_id")},
-                    loadSubtitles
-                )
-            });
-
-            $(".editsubs-repo-create", $videoElement).on("click", function(){
-                var $createRepoForm = $("#editsubs-repo-create-modal");
-                $createRepoForm.leanModal();
-
-                $(".es-repo-create-submit", $createRepoForm).on("click", function(){
-                    _requestHelper(
-                        "create_repository",
-                        {
-                            "name": $createRepoForm.find("#es-repo-name").text(),
-                            "description": $createRepoForm.find("#es-repo-desc").text(),
-                            "lang_tag": $createRepoForm.find("#es-lang-tag").val(),
-                            "is_private": $createRepoForm.find("#es-repo-private").is(":checked")
-                        },
-                        function(data){}
-                    );
-                    $(this).off("click");
-                });
-
-                $(".editsubs-repo-rate", $videoElement).on("click", function(){
-                    var $rateForm = $("#editsubs-rate-modal");
-                    $rateForm.leanModal();
-
-                    _requestHelper(
-                        "get_subtitles",
-                        {"repo_id": currentRepo},
-                        function(data){
-                            //TODO: make table
-                        }
-                    );
-                });
-
-
-            });
-
         });
 
-        /*
-            Remove all handlers while deactivating module
-         */
+
         $activateBtn.on("click",".es-activated", function(){
             $(".video").off("caption:update", updateSubtitle);
             $subtitlesBtn.css("display", "none");
             $defSubtitleBtn.css("display","block");
-
-            $(".editsubs-operation-cancel", $videoElement).off("click");
-            $(".editsubs-operation-save", $videoElement).off("click");
-            $(".editsubs-string", $videoElement).off('input');
-            $(".es-rate-positive:not(.es-rated)", $videoElement).off("click");
-            $(".es-rate-negative:not(.es-rated)", $videoElement).off("click");
-            $(".editsubs-repo", $videoElement).off("click");
-            $(".editsubs-repo-create", $videoElement).off("click");
-
             $(this).removeClass("es-activated").addClass("es-deactivated");
 
+
         });
+
+
+        $(".editsubs-operation-cancel", $videoElement).click(function(){
+            $subtitleString.text(changedSubtitleString);
+            isEditingActive = false;
+        });
+
+        $("editsubs-operation-save", $videoElement).click(function(){
+            _requestHelper(
+                "add_subtitle",
+                JSON.stringify({
+                    "text": $subtitleString.text(),
+                    "start": subtitleData.start[$subtitleString.data("index")],
+                    "end": subtitleData.end[$subtitleString.data("index")]
+                }),
+                function(data){}
+            )
+        });
+
+        $(".editsubs-string", $videoElement).on('input', function(){
+            if(isEditingActive) return;
+            $subtitleString = $(this);
+            changedSubtitleString = $subtitleString.text();
+            isEditingActive = true;
+        });
+
+        function rate(subtitleIndex, serverHandler){
+            _requestHelper(
+                serverHandler,
+                {
+                    "subtitle_id": subtitleData.subtitle_id[subtitleIndex]
+                },
+                function(data){}
+            )
+        }
+        $(".es-rate-positive:not(.es-rated)", $videoElement)
+            .on("click", function(){
+                rate($subtitleString.data("index"), "rate_positive");
+        });
+
+        $(".es-rate-negative:not(.es-rated)", $videoElement)
+            .on("click", function(){
+                rate($subtitleString.data("index"), "rate_negative");
+        });
+
+        $(".editsubs-repo", $videoElement).on("click", function(){
+            _requestHelper(
+                "switch_to_repository",
+                {"repo_id": $(this).data("repo_id")},
+                loadSubtitles
+            )
+        });
+
+        $(".editsubs-repo-create", $videoElement).on("click", function(){
+            var $createRepoForm = $("#editsubs-repo-create-modal");
+            $createRepoForm.leanModal();
+
+            $(".es-repo-create-submit", $createRepoForm).on("click", function(){
+                _requestHelper(
+                    "create_repository",
+                    {
+                        "name": $createRepoForm.find("#es-repo-name").text(),
+                        "description": $createRepoForm.find("#es-repo-desc").text(),
+                        "lang_tag": $createRepoForm.find("#es-lang-tag").val(),
+                        "is_private": $createRepoForm.find("#es-repo-private").is(":checked")
+                    },
+                    function(data){}
+                );
+                $(this).off("click");
+            });
+        });
+
+        $(".editsubs-repo-rate", $videoElement).on("click", function(){
+            var $rateForm = $("#editsubs-rate-modal");
+            $rateForm.leanModal();
+
+            _requestHelper(
+                "get_subtitles",
+                {"repo_id": currentRepo},
+                function(data){
+                    //TODO: make table
+                }
+            );
+        });
+
+        function subtitleUpdateHandler(data){
+            if(isEditingActive)
+                return $videoElement.trigger("deferredUpdate", data);
+            var index = subtitleData.start.indexOf(data.start);
+            if (index < 0) return;
+            if (subtitleData.rating[index] > data.rating) return;
+            else if (subtitleData.rating[index] === data.rating
+                     && new Date(subtitleData.updated_at[index]) > new Date(data.updated_at) )
+                return;
+            subtitleData.text[index] = data.text;
+            subtitleData.subtitle_id[index] = data.subtitle_id;
+            subtitleData.rating[index] = data.rating;
+            subtitleData.updated_at = data.updated_at;
+        }
+
+        function repositoriesUpdateHandler(data) {
+            if (!data.repo_id in repos.repo_id){
+                repos.repo_id.push(data.repo_id);
+                repos.repo_name.push(data.repo_name);
+                repos.description.push(data.description);
+            }
+            //TODO: Append to DOM
+        }
     });
 }
